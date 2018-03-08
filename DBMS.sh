6ba29577b6cb8009b91c6b2dd51e9faf18524894
KamalMagdy <kamal.m2331@yahoo.com>
2018-03-08 13:47:10 +0200

    create table & insert functions DOOOONE, with constrains

--- DBMS.sh
+++ DBMS.sh
@@ -2,6 +2,7 @@
 
 USEDB_FLAG=0
 DB_NAME=""
+# pkFlag=0
 
 function showDBs(){
   if [[ "${arr[0]}" = "show" && "${arr[1]}" = "databases" ]]; then
@@ -20,7 +21,61 @@ function createDBsTb(){
       echo "Database ${arr[2]} already exist!"
     else
       mkdir "${arr[2]}"
-      echo "Database ${arr[2]} created suceessfully"
+      echo "Database ${arr[2]} created successfully"
+    fi
+  elif [[ "${arr[0]}" = "create" && "${arr[1]}" = "table" ]]; then
+    if (( $USEDB_FLAG )); then
+      tableName="${arr[2]}"
+      if [ -f ./$DB_NAME/$tableName.table ]; then
+        echo "Table ${arr[2]} already exist!"
+      else
+        touch ./$DB_NAME/$tableName.table
+        touch ./$DB_NAME/$tableName.cons  #constrains
+        read -p "How many fields to create: " nf
+        x=1
+        while [ $x -le $nf ]; do
+          read -p "Field $x's Name: " fld
+          # if [[ $pkFlag == 0 ]]; then
+            read -p "Is it the Primary Key? (y/n) " pk
+            # pkFlag=1
+            if [[ $pk == "y" ]]; then      #unique, means not NULL and has no default value
+              unique="y"
+              notNull="y"
+              hasDef="n"
+            else
+              read -p "Is the field Unique? (y/n) " unique
+              if [ $unique = "y" ]; then  #not NULL and has no default value
+                notNull="y";
+                hasDef="n";
+              else
+                read -p "The filed is NOT NULL? (y/n) " notNull
+                read -p "Is the field has Default Value (y/n)? " hasDef
+                if [ $hasDef = "y" ]; then
+                  read -p "Enter Default value: " hasDef
+                fi
+              fi
+            fi
+          # fi
+          if [ $hasDef = "n" ];then
+            read -p "Enter Data Type (int - char - varchar ): " dataType
+            if [ $dataType = "int" ]; then
+              dataType="int";
+            elif [ $dataType = "char" ]; then
+              dataType="char"
+            else
+              dataType="varchar";
+            fi
+          fi
+          saveCons=" ";
+          saveCons="$fld:$pk:$notNull:$unique:$hasDef:$dataType";
+
+          echo "$saveCons" >> ./$DB_NAME/$tableName.cons
+          x=`expr $x + 1`;
+        done
+        echo "Table Created Successfully"
+      fi
+    else
+        echo "No DataBase used!"
     fi
   else
     echo "Syntax Error!"
@@ -31,7 +86,7 @@ function createDBsTb(){
 function useDB(){
   if [[ "${arr[0]}" = "use" && -d "${arr[1]}" ]]; then
     let "USEDB_FLAG=1";
-    let "DB_NAME=${arr[1]}";
+    declare -g "DB_NAME=${arr[1]}";
     echo "DataBase changed"
   else
     echo "Please enter a valid DataBase name"
@@ -40,42 +95,162 @@ function useDB(){
 
 #######################################
 function alterTable(){
-    #code here ...
+    echo "alter"
 }
 
 #######################################
 function dropTable(){
-    #code here ...
+    echo "drop"
 }
 
 #######################################
 function selectRecord(){
-    #code here ...
+    echo "select"
 }
 
 #######################################
+function uniqueRec(){
+  uniFlag=0;
+  noOfRec=`awk 'BEGIN{a=0;} { a++; } END{print a;}' $1`;
+  for (( i=1;i<=$noOfRec;i++ ))
+  do
+    firstRec=`awk -F ":" -v rec1=$i -v rec2=$2 ' NR == rec1 { print $rec2 } ' $1`;
+    for (( x=$i+1;x<=$noOfRec;x++ ))
+    do
+      secondRec=`awk -F ":" -v rec1=$x -v rec2=$2 ' NR == rec1 { print $rec2 } ' $1`;
+      if [ $firstRec = $secondRec ]; then
+        uniFlag=1;
+        break;
+      fi
+      if [ $firstRec = $3 ]; then
+        uniFlag=1;
+        break;
+      fi
+      if [ $secondRec = $3 ]; then
+        uniFlag=1;
+        break;
+      fi
+    done
+  done
+  echo $uniFlag;
+}
+###########insertRecord###########
 function insertRecord(){
-    #code here ...
+    if [[ ${arr[0]} = "insert" && ${arr[1]} = "into" ]]; then
+    tableName="./$DB_NAME/${arr[2]}.table"
+    consName="./$DB_NAME/${arr[2]}.cons"
+    if [ -f $consName ]; then
+      consFlag="y";
+      noRows=`awk 'BEGIN{i=0;} { i++; } END{print i;}' $consName`;
+      noColumns=`awk -F ":" ' NR == 1 {print NF}' $consName`;
+      x=1
+      while [ $x -le $noRows ]
+      do
+        i=0;
+        while [ $i -lt $noColumns ]
+        do
+          noOfLi=i;
+          i=`expr $i + 1`;
+          consR[noOfLi]=`awk -F ":" -v rec1=$x -v rec2=$i ' NR == rec1  { print $rec2  }' $consName`;
+        done
+
+        #$fld:$pk:$notNull:$unique:$hasDef:$dataType
+
+        echo "Field : ${consR[1]}"
+        read -r value[$x];
+        if [ ${consR[2]} = "y" ]; then   #Primary, NotNULL, unique, no default value
+          # xyz=`awk -F ";" 'NR == $x {p=$1; next} p == rec[$x] { "0" } { "1" }' $tableName`
+          if [ $( uniqueRec $tableName $x ${value[$x]} ) -eq 1 ]; then
+            echo "Primary Key must be unique"
+            consFlag="n"
+            break;
+          fi
+        fi
+        if [[ ${consR[3]} = "y" && $consFlag != "n" ]]; then    #NotNull
+          if [ ${value[$x]} = "" ]; then
+            consFlag="n";
+          fi
+        fi
+        if [[ ${consR[4]} = "y" && $consFlag != "n" ]]; then   #unique
+          ret=$( uniqueRec $tableName $x ${value[$x]} );
+          if [ $ret -eq 1 ]; then
+            echo "Unique field duplicated"
+            consFlag="n";
+            break;
+          fi
+        fi
+        if [[ ${consR[5]} != "n"  && $consFlag != "n" ]]; then  #Default value
+          if [[ ${value[$x]} == "" ]]; then
+            echo "${consR[5]}"
+            ${value[$x]}=${consR[5]}
+            echo "Default value inserted"
+          fi
+        fi
+
+        if [[ ${consR[5]} == "n" ]]; then
+          dataType=`echo "${consR[6]}" | awk -F ":" ' { print $1 } '`    #dataType
+          if [ $dataType == "int" ]; then
+            if [[ "${value[$x]}" != '' && "${value[$x]}" == *[0-9]* ]]; then
+              echo "Valid"
+            else
+              echo "Invalid, not Integer"
+              consFlag="n"
+              break;
+            fi
+          elif [ $dataType == "char" ]; then
+            if [[ "${value[$x]}" == *[a-zA-Z]* ]]; then
+              echo "Valid"
+            else
+              echo "Invalid, not alphabatical Characters"
+              consFlag="n"
+              break;
+            fi
+          else
+            echo "Valid";
+          fi
+        fi
+        x=`expr $x + 1`;
+      done
+      record="";
+      for (( a=1;a<=$noRows;a++ ))
+      do
+        if (( $a<$noRows ))
+        then
+          record="$record${value[$a]}:";
+        else
+          record="$record${value[$a]}";
+        fi
+      done
+      if [ $consFlag != "n" ]; then
+        echo "$record" | cat >> $tableName
+        echo "Values inserted successfully"
+      else
+        echo "Something is Wrong, check the constrains!"
+      fi
+    else
+      echo "Table doesn't exist!"
+    fi
+  fi
 }
 
 #######################################
 function updateRecord(){
-    #code here ...
+    echo "update"
 }
 
 #######################################
 function deleteRecord(){
-    #code here ...
+    echo "delete"
 }
 
 #######################################
 function sortTable(){
-    #code here ...
+    echo "sort"
 }
 
 #######################################
 function displayTable(){
-    #code here ...
+    echo "display"
 }
 
 #######################################
